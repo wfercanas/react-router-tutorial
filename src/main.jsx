103db132641fca0fc68bc6d6016e3cd74022b7ee
wfercanas <fernando.canas@outlook.com>
2022-12-13 14:23:51 -0500

    feat: Root component as element of root router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import Root from './routes/root'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>
+    element: <Root />
   }
 ])
 
