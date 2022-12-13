06630b1c7b0182bfaf34a35637b33e16dc9d90b3
wfercanas <fernando.canas@outlook.com>
2022-12-13 14:10:35 -0500

    feat: install vite app

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
