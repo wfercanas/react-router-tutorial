dc32c6a46e7c475b96bc00162acc58a305e98d8e
wfercanas <fernando.canas@outlook.com>
2022-12-14 17:06:39 -0500

    feat: use NavLink to mark which contact is in view

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,11 @@
-import { Link, Outlet, useLoaderData, Form, redirect } from "react-router-dom";
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useLoaderData,
+  Form,
+  redirect,
+} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
@@ -38,7 +45,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -47,7 +59,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
