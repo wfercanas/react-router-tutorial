c00ed952b3e717b35e6373c771110ffa53ccd95d
wfercanas <fernando.canas@outlook.com>
2022-12-15 17:22:41 -0500

    feat: optimistically switch star before confirm

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -66,7 +66,11 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   const fetcher = useFetcher();
+
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
 
   return (
     <fetcher.Form method="post">
